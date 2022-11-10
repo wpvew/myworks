c79780109cc642f93e96b4fa5e10c9bab3057bbd
wpvew <keitsutia@gmail.com>
2022-11-10 17:27:16 +0300

    update weather api

--- react/weather/src/shared/API/ApiServer.ts
+++ react/weather/src/shared/API/ApiServer.ts
@@ -1,26 +1,23 @@
 import axios from 'axios';
 import { getCapitalLetter } from '../utils/getCapitalLetter';
 
+const weatherApi =
+  'https://api.weatherapi.com/v1/forecast.json?key=f55e121d52a54c44b7b142420221011';
+
 export class ApiServer {
   static getForecastWeather(location: string) {
-    return axios.get(
-      `https://api.weatherapi.com/v1/forecast.json?key=120a103ec8454d949cf141146222410&q=${location}&days=9&aqi=no&alerts=no`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    return axios.get(`${weatherApi}&q=${location}&days=9&aqi=no&alerts=no`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
   static getLocationsWeather(location: string) {
-    return axios.get(
-      `https://api.weatherapi.com/v1/forecast.json?key=120a103ec8454d949cf141146222410&q=${location}&days=9&aqi=no&alerts=no`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    return axios.get(`${weatherApi}&q=${location}&days=9&aqi=no&alerts=no`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
   static getCities(value: string) {
     // axios.get('https://nominatim.openstreetmap.org/search?city=москв&format=json&addressdetails=1')
