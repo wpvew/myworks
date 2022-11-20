e9f98b94da254027d3a8308e677b65817b2e1e0a
wpvew <keitsutia@gmail.com>
2022-11-20 22:48:11 +0300

    init kris

--- react/forgit/client/src/utils/assoc.js
+++
@@ -1,6 +0,0 @@
-export function assoc(key, value) {
-  return (obj) => ({
-    ...obj,
-    [key]: value,
-  });
-}
--- react/forgit/server/routes/subjects.route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const SubjectsController = require('../controllers/SubjectController');
-
-const router = Router();
-
-router.get('/', SubjectsController.getSubjects);
-
-module.exports = router;
